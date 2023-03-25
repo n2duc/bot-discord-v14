6a24407f123e9bd4197abe1a10f59fcc87156fb0
Nguyen Duc <97241208+n2duc@users.noreply.github.com>
2023-03-25 20:22:39 +0700

    bug

--- Events/interactions/interactionCreate.js
+++ Events/interactions/interactionCreate.js
@@ -7,7 +7,7 @@ module.exports = {
      * @param {CommandInteraction} interaction
      */
     execute(interaction, client) {
-        const { commandName } = interaction;
+        const { commandName, customId } = interaction;
 
         if (interaction.isChatInputCommand()) {
             const command = client.commands.get(commandName);
@@ -20,15 +20,18 @@ module.exports = {
             command.execute(interaction, client);
         }
         if (interaction.isButton()) {
-            const role = interaction.guild.roles.cache.get(
-                "1087304182341570560"
-            );
-            return interaction.member.roles.add(role).then((member) =>
+            if (customId == "verify") {
+
+                const role = interaction.guild.roles.cache.get(
+                    "1087304182341570560"
+                    );
+                    return interaction.member.roles.add(role).then((member) =>
                 interaction.reply({
                     content: `${role} has been assigned to you.`,
                     ephemeral: true,
                 })
-            );
+                );
+            }
         } else {
             return;
         }
